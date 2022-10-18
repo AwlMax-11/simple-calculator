5f8d31efc2e767303a10bdf24fb39e8ce45c0e9c
AwlMax <maxshilo159@gmail.com>
2022-10-18 08:29:28 +0300

    Second commit

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-const express = require('express')
-const app = express()
-const bodyparser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use(bodyparser.urlencoded({encoded: true}))
+const app = express();
+const port = 3000;
 
-app.get('', (req, res) => {
-  res.sendFile(__dirname+"/index.html")
-})
+var errorMessage = "<h1>404</h1><p>Sorry it seems there was an error in the input you gave, please try again.</p>";
 
-app.post('/', (req, res) => {
-    const result1 = Number(req.body.num1)
-    const result2 = Number(req.body.num2)
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const add = result1 + result2
-    res.send("result: " + add)
-})
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(3000)
+app.post("/", function(req, res){
+
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var operator = req.body.op;
+
+  function calculate(n1, n2, op) {
+    var result = 0;
+
+    if (operator == "+") {
+      result = n1+n2;
+      res.send("The sum of " + n1 + " and " + n2 + " is " + result);
+    } else if (operator == "-") {
+      result = n1-n2;
+      res.send("The difference of " + n1 + " and " + n2 + " is " + result);
+    } else if (operator == "/") {
+      result = n1/n2;
+      res.send("The quotient of " + n1 + " and " + n2 + " is " + result);
+    } else if (operator == "*") {
+      result = n1*n2;
+      res.send("The product of " + n1 + " and " + n2 + " is " + result);
+    } else {
+      res.send(errorMessage);
+    }
+  }
+
+  calculate(num1, num2, operator);
+
+});
+
+app.listen(port, function(){
+  console.log("Server started properly on port " + port + ".");
+});
